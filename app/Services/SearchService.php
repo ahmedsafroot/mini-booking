f97f04052f8670ba3d12b9ab18a0cd6af97e4b3d
Ahmed Refaat <ahmed_rabdelrahman@etax.com.eg>
2026-02-27 22:52:41 +0200

    apply caching on search api

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -4,32 +4,37 @@ namespace App\Services;
 
 use App\Models\Hotel;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class SearchService
 {
     public function search(array $data)
     {
-        $nights = Carbon::parse($data['checkin_date'])
-            ->diffInDays($data['checkout_date']);
+        $cacheKey = 'search_' . md5(json_encode($data));
 
-        $hotels = Hotel::where('city', $data['city'])
-            ->whereHas('rooms', function ($q) use ($data) {
-                $q->where('max_occupancy', '>=', $data['guests'])
-                    ->where('available_rooms', '>', 0);
-            })
-            ->with(['rooms' => function ($q) use ($data) {
-                $q->where('max_occupancy', '>=', $data['guests'])
-                    ->where('available_rooms', '>', 0);
-            }])
-            ->get();
+        return Cache::remember($cacheKey, now()->addMinutes(5), function () use ($data) {
 
-        return $hotels->map(function ($hotel) use ($nights) {
-            $hotel->rooms->map(function ($room) use ($nights) {
-                $room->total_price =
-                    $room->price_per_night * $nights;
-                return $room;
+            $nights = Carbon::parse($data['checkin_date'])
+                ->diffInDays($data['checkout_date']);
+
+            $hotels = Hotel::where('city', $data['city'])
+                ->whereHas('rooms', function ($q) use ($data) {
+                    $q->where('max_occupancy', '>=', $data['guests'])
+                        ->where('available_rooms', '>', 0);
+                })
+                ->with(['rooms' => function ($q) use ($data) {
+                    $q->where('max_occupancy', '>=', $data['guests'])
+                        ->where('available_rooms', '>', 0);
+                }])
+                ->get();
+
+            return $hotels->map(function ($hotel) use ($nights) {
+                $hotel->rooms->map(function ($room) use ($nights) {
+                    $room->total_price = $room->price_per_night * $nights;
+                    return $room;
+                });
+                return $hotel;
             });
-            return $hotel;
         });
     }
 }
